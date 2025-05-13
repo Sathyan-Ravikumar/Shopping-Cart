8df0b3cb16357cda5b42f77745c8a31db914ecf2
Sathyan RaviKumar <sathyan.ravikumar@kanini.com>
2025-05-13 23:17:57 +0530

    added keyvault for db connection

--- Backend/ProductModule/Products Module/Program.cs
+++ Backend/ProductModule/Products Module/Program.cs
@@ -1,3 +1,10 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Products.Domain.Repositories;
+using Products.Domain.RepositoryInterfaces;
+using Products.Modal.DataAccess;
+using System.Data.Common;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +14,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddSingleton<SqlServerConnection>();
+builder.Services.AddScoped<IStoredProcedures, StoredProcedures>();
+
 var app = builder.Build();
+
+var keyVaultUrl = builder.Configuration["KeyVault:KeyVaultURL"];
+var secretClient = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential());
+
+var dbConnection = secretClient.GetSecret("SQLConnection").Value.Value;
+
+builder.Configuration["ConnectionStrings:SQLConnection"] = dbConnection;
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
